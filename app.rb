a277763bf51243db19b80e2ad029833e48e83854
Fernando Montero Pérez <Fernando Montero Prez>
2016-03-07 13:01:50 +0000

    Started working on the user signup

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
+require 'erb'
 require 'sinatra'
 require 'sqlite3'
 require_relative 'customer'
+
+include ERB::Util
 
 get '/' do
 
@@ -19,6 +22,17 @@ end
 
 get '/login' do
     erb :access
+end
+
+post '/user_signup' do
+    @firstname = params[:inputName].strip
+    @surname = params[:inputSurname].strip
+    @twitterUname = params[:inputTwitter].strip
+    @email = params[:inputEmail2].strip
+    @password = params[:inputPassword].strip
+
+
+    erb :index
 end
 
 get '/admin' do
