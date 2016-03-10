19098ebed29af7093f454163c071b0c10c4f65e5
Alex <afparker1@sheffield.ac.uk>
2016-03-10 20:27:24 +0000

    improvements/corrections to menu

--- app.rb
+++ app.rb
@@ -17,10 +17,6 @@ get '/' do
   erb :index
 end
 
-get '/menu' do
-  erb :menu
-end
-
 get '/about' do
   erb :about
 end
--- menu.rb
+++ menu.rb
@@ -1,8 +1,20 @@
 require 'sqlite3'
+require 'sinatra'
+require 'erb'
 
-@db = SQLite3::Database.new './curry_house.sqlite'
+before do
+  @db = SQLite3::Database.new './curry_house.sqlite'
+end
 
-query = %{SELECT itemName, description, unitPrice
+get '/menu' do
+  query = %{SELECT itemName, description, unitPrice
                FROM menu
                WHERE category = ? }
-@sidesResults = @db.execute query, 'side'
+
+  @sidesResults = @db.execute query, 'side'
+  @starterResults = @db.execute query, 'starter'
+  @mildResults = @db.execute query, 'mild'
+  @hotResults = @db.execute query, 'hot'
+
+  erb :menu
+end
