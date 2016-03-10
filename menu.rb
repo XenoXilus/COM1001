79ad190289fe4938905ee3dcef9d0d7ca2fd6fb8
Alex <afparker1@sheffield.ac.uk>
2016-03-10 18:37:05 +0000

    Added category and vegetarian fields to menu table, added more placeholder items and started on controller file

--- menu.rb
+++ menu.rb
@@ -1,4 +1,8 @@
-before do
-  @db = SQLite3::Database.new './curry_house.sqlite'
-end
+require 'sqlite3'
+
+@db = SQLite3::Database.new './curry_house.sqlite'
+
+sidesQuery = %{SELECT itemName, unitPrice, description FROM menu WHERE category = 'side'}
+@sidesResults = @db.execute sidesQuery
+puts @sidesResults
 
