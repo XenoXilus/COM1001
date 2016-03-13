1f251b321f19d7ed0bbe753e2882931e93d70f6e
Alexandros Gkigkolian <Alexandros Gkigkolian>
2016-03-13 23:37:18 +0000

    Signup/login ready

---
+++ LogIn.rb
@@ -0,0 +1,68 @@
+require 'sinatra'
+require 'erb'
+require 'sqlite3'
+require 'tilt/erb'
+
+include ERB::Util
+
+before do
+  @db = SQLite3::Database.new('./Data.sqlite')
+end
+
+enable :sessions
+set :session_secret, 'super secret'
+
+
+
+get '/' do
+  redirect '/login' unless session[:logged_in]
+  @submitted = false
+  erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+
+
+post '/login' do
+
+  @submitted = true
+
+  #####check
+
+  email_address=params[:email_address].strip
+
+  pswrd=params[:password].strip
+
+
+
+
+
+  name=@db.execute('SELECT Firstname FROM Info WHERE Email_address = ? ',[email_address])
+  @email_address_ok=!name[0][0].nil?
+    surname = @db.execute('SELECT surname FROM Info WHERE Email_address = ? AND Password=?',[email_address,pswrd])
+
+  @password__ok = !surname[0].nil?
+  puts @email_address_ok
+  puts @password__ok
+  puts pswrd
+
+
+  @everything_ok=@password__ok&&@email_address_ok
+
+  if @everything_ok
+    session[:logged_in] = true
+    session[:login_time] = Time.now
+    redirect '/'
+  end
+  @error = "The Email address or the password is not correct"
+  erb :login
+end
+
+get '/logout' do
+  session.clear
+  erb :logout
+end
+
--- SignUp.rb
+++ SignUp.rb
@@ -1,3 +1,8 @@
+require 'erb'
+require 'sinatra'
+require 'sqlite3'
+
+include ERB::Util
 
 before do
   @db = SQLite3::Database.new('curry_house.sqlite')
@@ -5,13 +10,14 @@ end
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
-get '/signup' do
+get '/' do
   @submitted = false
   erb :signUpForm
 end
 
 post '/form_handler' do
   @submitted = true
+  @email_address = params[:email_address].strip
 
   @firstname = params[:firstname].strip
   @surname = params[:surname].strip
@@ -24,25 +30,23 @@ post '/form_handler' do
       !@firstname.nil? && @firstname != ""
   @surname_ok = !
   @surname.nil? && @surname != ""
+
   @email_address_ok =
       !@email_address.nil? && @email_address =~ VALID_EMAIL_REGEX
+
+    @check_emails=@db.execute('SELECT*FROM Info WHERE Email_address = ?LIMIT 1 ',[@email_address])
+  @different_emails_ok=@check_emails[0].nil? || @check_emails[0]==""
+
   @password_ok =
       !@password.nil? && @password !="" && @password.length>=6
   @confirm_password_ok =
       @confirm_password==@password
 
-  @all_ok = @firstname_ok && @surname_ok && @email_address_ok&& @password_ok&& @confirm_password_ok
-
+  @all_ok = @firstname_ok && @surname_ok && @email_address_ok&& @password_ok&& @confirm_password_ok &&@different_emails_ok
 
 
   if @submitted && @all_ok
     @db.execute('INSERT INTO Info VALUES(?, ?, ?, ?)', [@firstname, @surname, @email_address, @password])
-
-@check_email="not_same"
-
-  @check_emails=@db.execute('SELECT * FROM Info WHERE Email_address=? LIMIT 1 '),[@email_address]
-
-  puts @check_email
 end
   erb :signUpForm
 end
