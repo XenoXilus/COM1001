7471655c98c422693de4016acec201e3442432ad
Alexandros Gkigkolian <Alexandros Gkigkolian>
2016-03-14 21:00:09 +0000

    Created tweets table, added changing status order feature + refactoring.

--- curry_house_twitter.rb
+++ curry_house_twitter.rb
@@ -2,7 +2,6 @@ require 'twitter'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-
 # tweets = client.search('curry house')
 # most_recent = tweets.take(15)
 # most_recent.each do |tweet|
@@ -14,9 +13,7 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 #   end
 # end
 
-
-def search_for_orders
-
+def init
   config = {
       :consumer_key => 'NNtgfDPbs3e2RfYogLwozovGn',
       :consumer_secret => 'rZAtPRt3IlTJ0gUjMALihOYbDCqytVhse58lGPgn863gq5oLss',
@@ -24,26 +21,39 @@ def search_for_orders
       :access_token_secret => 'ZJGwvPMmkwV5f6YaVZrUVgztiGcV1iXupncKGmqhXKXWb'
   }
 
-  client = Twitter::REST::Client.new(config)
-  tweets = client.mentions_timeline()
+  $client = Twitter::REST::Client.new(config)
+end
+
+def search_for_orders
+
+
+  tweets = $client.mentions_timeline()
   most_recent = tweets.take(5)
 
+
+
+  #array_has_on_col(@caught_tweets,123,0)
   most_recent.each do |tweet|
     if !tweet.text.include? 'RT'
-      if tweet.text.include?('order') && (@caught_tweets.nil? || (!@caught_tweets.nil? && !@caught_tweets.any? {|h| h[:id]==tweet.id}))
+      if tweet.text.include?('order') && (@caught_tweets.nil? || (!@caught_tweets.nil? && !array_has_on_col(@caught_tweets,tweet.id,0)))
         puts "Order: #{tweet.text}"
         client.favorite(tweet.id)
-        tweet_hash = {
-            :id => tweet.id,
-            :from => tweet.attrs[:user][:screen_name],
-            :text => tweet.text,
-            :status => 'Ordered'
-        }
-        @caught_tweets.push(tweet_hash)
+        tweet_arr = [tweet.id,tweet.attrs[:user][:screen_name],tweet.text,'Ordered']
+        @db.execute('INSERT INTO tweets VALUES (?,?,?,?)',tweet_arr)
+        @caught_tweets.push(tweet_arr)
         client.update("Hi @#{tweet.attrs[:user][:screen_name]}! Your order has been accepted. To cancel go to our website!", :in_reply_to_status_id => tweet.id)
       end
     end
   end
+end
+
+def array_has_on_col(arr,elem,c)
+  arr.each do |r|
+    if r[c] == elem
+      return true
+    end
+  end
+  return false
 end
 
 def competition
@@ -56,3 +66,5 @@ def offers
   discount = "CH#{rand_number}"
   client.update("Use discount code : #{discount} for money off at the checkout!")
 end
+
+init
--- orders.rb
+++ orders.rb
@@ -1,13 +1,32 @@
 before do
-  @caught_tweets = Array.new
-
+  #@caught_tweets = Array.new
+  @db = SQLite3::Database.new './curry_house.sqlite'
+  @caught_tweets = @db.execute('SELECT * FROM tweets')
 end
 
 get '/orders' do
+
   begin
     search_for_orders
   rescue Twitter::Error::TooManyRequests => err
     puts "TooManyRequests, try again in #{err.rate_limit.reset_in} seconds"
+  end
+
+  erb :orders
+end
+
+post '/change_status' do
+  puts params
+  puts params["status1"]
+  puts @caught_tweets.class
+  puts params[:change_to]
+  for i in 0..@caught_tweets.size-1 do
+    puts params["status#{i}"]
+    if params["status#{i}"]=="on"
+      #update both db and array
+      @db.execute('UPDATE tweets SET status = ? WHERE id = ?',[params[:change_to],@caught_tweets[i][0]])
+      @caught_tweets[i][3] = params[:change_to]
+    end
   end
 
   erb :orders
