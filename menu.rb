24ecce3d0dd0e316111a10b1593c81f7140a4c20
Alex <afparker1@sheffield.ac.uk>
2016-04-28 18:01:51 +0100

    changed my instances of itemNum to id and removed the field further refactoring of menu code

--- menu.rb
+++ menu.rb
@@ -6,16 +6,16 @@ before do
   @db = SQLite3::Database.new './curry_house.sqlite'
 	#todo get session location!
 	session[:sheffield] = TRUE
-	session[:birmingham] = TRUE
+	session[:birmingham] = FALSE
 end
 
 get '/menu' do
 	if session[:birmingham]
-    query = %{SELECT itemName, description, unitPrice, vegetarian, glutenFree, itemNum
+    query = %{SELECT itemName, description, unitPrice, vegetarian, glutenFree, id
               FROM menu
               WHERE category = ? AND atBirm = 1 }
 	else
-		query = %{SELECT itemName, description, unitPrice, vegetarian, glutenFree, itemNum
+		query = %{SELECT itemName, description, unitPrice, vegetarian, glutenFree, id
               FROM menu
               WHERE category = ? AND atSheff = 1 }
   end
@@ -25,6 +25,14 @@ get '/menu' do
   @hotResults = @db.execute query, 'hot'
   @riceResults = @db.execute query, 'rice'
   @sidesResults = @db.execute query, 'side'
+
+	Array @m = [
+			["Starters", @starterResults],
+	    ["Mild Dishes", @mildResults],
+	    ["Hot Dishes", @hotResults],
+	    ["Rice Dishes", @riceResults],
+	    ["Sides", @sidesResults]
+	]
 
   erb :menu
 end
