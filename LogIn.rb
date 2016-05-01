c6a74af08dde9fa32f55eb91a5be4c3e35763ed6
aca15am <amarketos1@sheffield.ac.uk>
2016-05-01 20:51:27 +0100

    some improvements to login-signUp form

--- LogIn.rb
+++ LogIn.rb
@@ -10,7 +10,7 @@ before do
 end
 
 enable :sessions
-set :session_secret, 'super secret'
+
 
 get '/login' do
   @logSubmitted = false
@@ -27,19 +27,16 @@ post '/login' do
 
   @logSubmitted = true
 
-  email_address = params[:email_address].strip
+  @email_address = params[:email_address].strip
   pswrd = params[:password].strip
 
-  puts email_address
-  puts pswrd
-
-  name = @db.execute('SELECT firstName FROM customer WHERE email = ? ', email_address)
+  name = @db.execute('SELECT firstName FROM customer WHERE email = ? ', @email_address)
   @email_address_ok = !name[0].nil?
 
   if @email_address_ok
-    surname = @db.execute('SELECT surname FROM customer WHERE email = ? AND password = ?', email_address, pswrd)
+    surname = @db.execute('SELECT surname FROM customer WHERE email = ? AND password = ?', @email_address, pswrd)
     @password_ok = !surname[0].nil?
-    @blacklisted = @db.get_first_value('SELECT blacklisted FROM customer WHERE email = ?',email_address) == 1
+    @blacklisted = @db.get_first_value('SELECT blacklisted FROM customer WHERE email = ?',@email_address) == 1
   end
 
 
@@ -48,13 +45,13 @@ post '/login' do
 
   if @everything_ok
     session[:logged_in] = true
-    session[:city]= @db.get_first_value('SELECT city FROM customer WHERE email = ?',email_address)
+    session[:city]= @db.get_first_value('SELECT city FROM customer WHERE email = ?',@email_address)
     Stats.increment 'logins',session[:city]
     session[:login_time] = Time.now
-    session[:email] = email_address
-    session[:twitter_acc]  = @db.get_first_value('SELECT twitterAcc FROM customer WHERE email = ?',email_address)
+    session[:email] = @email_address
+    session[:twitter_acc]  = @db.get_first_value('SELECT twitterAcc FROM customer WHERE email = ?',@email_address)
 
-    admin_ok =  !(@db.get_first_value('SELECT email FROM administrators WHERE email = ?',email_address)).nil?
+    admin_ok =  !(@db.get_first_value('SELECT email FROM administrators WHERE email = ?',@email_address)).nil?
 
 
     if admin_ok then
